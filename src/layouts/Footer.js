03959958bc07ddbf892dd1b33648504cbc6df239
lalao123 <2432458797@qq.com>
2019-08-16 11:22:35 +0800

    'init'

---
+++ src/layouts/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class App extends React.Component{
+    render(){
+        return (
+            <div className='footer'>
+                 <h3>footer</h3>
+            </div>
+        )
+    }
+}
